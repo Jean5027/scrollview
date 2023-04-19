2aaf246d8cdad740343afbd23fbda948f75d1178
Jean5027 <112779029+Jean5027@users.noreply.github.com>
2023-04-19 16:30:52 -0300

    primeiro commit
    taaaaaaaa

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Post from './src/screens/Post';
+import { View, Text } from 'react-native'
+
+export default function App() {
+  return (
+    <View>
+      <Post></Post>
+    </View>
+    
+  );
+}
---
+++ src/screens/Post/index.jsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export default function Post() {
+
+  return(
+    <View style={styles.container}>
+
+    <Text style={styles.title}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero possimus qui
+      quod, nihil reiciendis adipisci nisi. Eaque placeat ab nobis eum, ea at
+      deserunt similique ut laudantium vitae, laboriosam consequuntur!
+    </Text>
+    
+    </View>
+  )
+  
+}
